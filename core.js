e6d2cc3d82789da12b255f4bbad9840615015bed
GhostDolphin <d.ghost2ac@gmail.com>
2023-05-22 14:20:24 +0300

    Restructured code

--- core.js
+++ core.js
@@ -1,41 +1,41 @@
 'use strict';
 
 const Node = class {
-	constructor(char, pos, prev, next) {
-		this.char = char;
-		this.pos = pos;
-		this.prev = prev;
-		this.next = next;
-	}
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
 };
 
 const List = class {
-	constructor() {
-		this.head = null;
-		this.body = [];
-		this.tail = null;
-	}
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.inLength = 0;
+  }
 
-	length() {
-		if (!this.head) return 0;
-		if (!this.tail) return 1;
-		return this.tail.pos + 1;
-	}
+  length() {
+    return this.inLength;
+  }
 
-	append(char) {
-		const len = this.length();
-		if (len === 0) {
-			this.head = new Node(char, 0, null, null);
-			this.tail = this.head;
-		}
-		if (len === 1) {
-			this.tail = new Node(char, 1, this.head.char, null);
-			this.head.next = this.tail.char;
-		}
-		if (len > 1) {
-			this.tail.next = char;
-			this.body[this.body.length] = this.tail;
-			this.tail = new Node(char, len, this.tail.char, null);
-		}
-	}
+  append(element) {
+    const newNode = new Node(element);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.inLength++;
+  }
 };
+
+// const test = new List();
+
+// test.append('a');
+// console.log(test);
