b750b31a1d4eb21305ba8b3ea3fee2ba2f886041
GhostDolphin <d.ghost2ac@gmail.com>
2023-05-22 16:44:16 +0300

    Added tests of each methods

--- core.js
+++ core.js
@@ -64,3 +64,53 @@ const List = class {
     this.elements = [...this.elements, ...elements.elements];
   }
 };
+
+const list = new List();
+
+// Test append
+list.append("H");
+list.append("W");
+console.log(list.elements);
+
+// Test insert
+list.insert("I", 1);
+console.log(list.elements);
+
+// Test delete
+let deletedElement = list.delete(1);
+console.log(list.elements);
+console.log(deletedElement);
+
+// Test deleteAll
+list.append("H");
+list.deleteAll("H");
+console.log(list.elements);
+
+// Test get
+list.append("G");
+console.log(list.get(1));
+
+// Test clone
+let cloneList = list.clone();
+console.log(cloneList.elements);
+
+// Test reverse
+list.reverse();
+console.log(list.elements);
+
+// Test findFirst
+console.log(list.findFirst("World"));
+
+// Test findLast
+console.log(list.findLast("World"));
+
+// Test clear
+list.clear();
+console.log(list.elements);
+
+// Test extend
+let newList = new List();
+newList.append("New");
+newList.append("List");
+list.extend(newList);
+console.log(list.elements);
