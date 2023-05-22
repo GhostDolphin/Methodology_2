250603cb76c1a3d879d1cebceda7d10b0054f895
GhostDolphin <d.ghost2ac@gmail.com>
2023-05-22 14:21:24 +0300

    Added insert() method

--- core.js
+++ core.js
@@ -33,6 +33,44 @@ const List = class {
 
     this.inLength++;
   }
+
+  insert(element, index) {
+    if (index < 0 || index > this.inLength) {
+      throw new Error("Invalid index");
+    }
+
+    const newNode = new Node(element);
+
+    if (index === 0) {
+      if (this.head === null) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+      }
+    } else if (index === this.inLength) {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      let currentNode = this.head;
+      let currentIndex = 0;
+
+      while (currentIndex < index - 1) {
+        currentNode = currentNode.next;
+        currentIndex++;
+      }
+
+      newNode.next = currentNode.next;
+      newNode.prev = currentNode;
+      currentNode.next.prev = newNode;
+      currentNode.next = newNode;
+    }
+
+    this.inLength++;
+  }
 };
 
 // const test = new List();
